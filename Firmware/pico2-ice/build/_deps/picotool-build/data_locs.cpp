89313d74271e7cd9ea175d5be40e62666901682b
frohro <rob.frohne@wallawalla.edu>
2025-09-13 00:20:06 +0000

    No changes made to the source code.

---
+++ Firmware/pico2-ice/build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/frohro/Classes/Digital_Design/logicanalyzer/Firmware/pico2-ice/build/_deps/picotool/"
+};
